c6d7dfe3861894d6c37cd7e53f72cb9b8f848d51
Beymer-pyg <beymerpyg@gmail.com>
2024-05-04 10:37:22 -0500

    navbar v1

---
+++ src/components/ui/NavItem.tsx
@@ -0,0 +1,42 @@
+import { PRODUCT_CATEGORIES } from "@/config";
+import React from "react";
+import { Button } from "./button";
+import { ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+type Category = (typeof PRODUCT_CATEGORIES)[number];
+
+interface NavItemProps {
+  category: Category;
+  handleOpen: () => void;
+  close: () => void;
+  isOpen: boolean;
+  isAnyOpen: boolean;
+}
+
+export const NavItem = ({
+  isAnyOpen,
+  category,
+  handleOpen,
+  isOpen,
+  close,
+}: NavItemProps) => {
+  return (
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? "secondary" : "ghost"}
+        >
+          {category.label}
+          <ChevronDown
+            className={cn("h-4 w-4 transition-all text-muted-foreground", {
+              "-rotate-180": isOpen,
+            })}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+};
